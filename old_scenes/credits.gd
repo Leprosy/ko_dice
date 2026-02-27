3a6d380fd8aa76203325c42bd904964e0969bf0c
Miguel Rojas <leprosy57@gmail.com>
2026-02-27 00:55:04 -0300

    Removed old code

--- old_scenes/credits.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-func _ready() -> void:
-    #self.get_parent().get_node("AudioStreamPlayer - Music").stop()
-    $AudioStreamPlayer.play()
-
-func _on_button_pressed() -> void:
-    #self.get_parent().get_node("AudioStreamPlayer - Music").play_rnd()
-    self.get_parent().change_scene_to_node(self, "main_menu")
