b44a7b2c3cdd43be2561b839e8f76e15615d1d2e
Miguel Rojas <leprosy57@gmail.com>
2026-04-12 21:21:36 -0400

    Working on show perks

--- scenes/components/control_panel.gd
+++ scenes/components/control_panel.gd
@@ -9,11 +9,10 @@ func _on_visibility_changed() -> void:
     print(State.perks)
     print(self.visible) # Replace with function body.
     
-    var i = 0
     for perk_item in State.perks:
         print(perk_item)
-        var perk_instance = perk.instantiate()
-        $Panel/ScrollContainer.add_child(perk_instance)
-        perk_instance.get_perk(perk_item.perk_name)
-        perk_instance.set_pos(60, 250 + 140 * i)
-        i += 1
+        #var perk_instance = perk.instantiate()
+        var item = Button.new()
+        item.text = perk_item.perk_name
+        $Panel/ScrollContainer/VBoxContainer.add_child(item)
+        #perk_instance.get_perk(perk_item.perk_name)
