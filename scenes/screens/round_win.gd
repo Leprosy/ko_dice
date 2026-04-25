1c957189882fe92fd7417eb517b985945696bb01
Miguel Rojas <leprosy57@gmail.com>
2026-04-25 17:59:27 -0400

    Perks!

--- scenes/screens/round_win.gd
+++ scenes/screens/round_win.gd
@@ -1,8 +1,10 @@
 extends Screen
 
 var perk = preload("res://scenes/components/perk.tscn")
-const PERK_ANGLE = PI/9
-const VERT_FIX = 20
+var selected_perk = -1
+const PERK_ANGLE = PI/14
+const HOR_FIX = 70
+const VERT_FIX = 10
 
 func _ready() -> void:
     var state = get_tree().root.get_node("Main/State")
@@ -13,18 +15,34 @@ func _ready() -> void:
     var i = 0
     for perk_name in available_perks:
         var perk_instance = perk.instantiate()
-        self.add_child(perk_instance)
+        $Perks.add_child(perk_instance)
         perk_instance.get_perk(perk_name)
-        perk_instance.set_pos(60 + 50 * i, 250)
+        perk_instance.set_pos(40 + HOR_FIX * i, 250 + VERT_FIX * i)
         perk_instance.get_child(0).set_rotation(-PERK_ANGLE + PERK_ANGLE * i)
-        perk_instance.perk_clicked.connect(on_perk_clicked.bind(perk_instance))
+        perk_instance.perk_clicked.connect(on_perk_clicked.bind(perk_instance, i))
         i += 1
 
 func _on_button_pressed() -> void:
     self.Main.set_active_scene("game")
 
-func on_perk_clicked(perk_clicked):
-    var state = get_tree().root.get_node("Main/State")
-    var data = inst_to_dict(perk_clicked)
-    state.perks.push_back(data)
-    self.Main.set_active_scene("game")
+func on_perk_clicked(perk_clicked, index):
+    var i = 0
+    for perk in $Perks.get_children():
+        perk.get_child(0).z_index = 0
+        perk.get_child(0).set_rotation(-PERK_ANGLE + PERK_ANGLE * i)
+        perk.set_pos(40 + HOR_FIX * i, 250 + VERT_FIX * i)
+        i+=1
+    
+    if selected_perk == index:
+        var state = get_tree().root.get_node("Main/State")
+        var data = inst_to_dict(perk_clicked)
+        state.perks.push_back(data)
+        self.Main.set_active_scene("game")
+    else:
+        var old_pos = perk_clicked.get_child(0).position
+        print(old_pos)
+        var card = perk_clicked.get_child(0)
+        card.z_index = 10
+        card.set_rotation(0)
+        perk_clicked.set_pos(old_pos.x, old_pos.y - 60)
+        selected_perk = index
