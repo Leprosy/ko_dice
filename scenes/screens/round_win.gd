19ac0dea9d1af1da98ac708d68e3478aaba707d3
Miguel Rojas <leprosy57@gmail.com>
2026-05-01 18:59:21 -0400

    Nice pick ui

--- scenes/screens/round_win.gd
+++ scenes/screens/round_win.gd
@@ -3,6 +3,7 @@ extends Screen
 var perk_scn = preload("res://scenes/components/perk.tscn")
 var selected_perk = -1
 const HOR_FIX = 100
+const SCALE = 0.8
 
 func _ready() -> void:
     var state = get_tree().root.get_node("Main/State")
@@ -16,30 +17,33 @@ func _ready() -> void:
         $Perks.add_child(perk_instance)
         perk_instance.get_perk(perk_name)
         perk_instance.set_pos(10 + HOR_FIX * i, 250)
-        
         perk_instance.perk_clicked.connect(on_perk_clicked.bind(perk_instance, i))
+        unselect_card(perk_instance.get_child(0))
         i += 1
 
 func _on_button_pressed() -> void:
     self.Main.set_active_scene("game")
 
 func on_perk_clicked(perk_clicked, index):
+    $Control/Panel/Help.visible = true
     for perk in $Perks.get_children():
-        perk.get_child(0).z_index = 0
-
+        unselect_card(perk.get_child(0))
     if selected_perk == index:
-        print("SELECTING", index)
-        return
         var state = get_tree().root.get_node("Main/State")
         var data = inst_to_dict(perk_clicked)
         state.perks.push_back(data)
         self.Main.set_active_scene("game")
     else:
-        var old_pos = perk_clicked.get_child(0).position
-        print(old_pos)
         $Perks.move_child(perk_clicked, 0)
-        var card = perk_clicked.get_child(0)
-        card.z_index = 10
-        #card.set_rotation(0)
-        #perk_clicked.set_pos(old_pos.x, old_pos.y - 60)
+        select_card(perk_clicked.get_child(0))
         selected_perk = index
+
+func select_card(card):
+    card.z_index = 10
+    card.scale.x = 1
+    card.scale.y = 1
+
+func unselect_card(card):
+    card.z_index = 0
+    card.scale.x = SCALE
+    card.scale.y = SCALE
