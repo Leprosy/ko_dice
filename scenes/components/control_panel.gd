339f26db3020385fbc7aee1636b7f1f3f21c06ea
Miguel Rojas <leprosy57@gmail.com>
2026-05-02 17:42:02 -0400

    Hands data

--- scenes/components/control_panel.gd
+++ scenes/components/control_panel.gd
@@ -6,13 +6,18 @@ func _on_visibility_changed() -> void:
     if not self.visible:
         return
 
-    for child in $Panel/ScrollContainer/VBoxContainer.get_children():
+    for child in $TabContainer/Options/ScrollContainer/VBoxContainer.get_children():
         child.queue_free()
-    var state = get_tree().root.get_node("Main/State")    
+    var state = get_tree().root.get_node("Main/State")
     for perk_item in state.perks:
         var item = Button.new()
         item.text = perk_item.perk_name
         $Panel/ScrollContainer/VBoxContainer.add_child(item)
+
+    var hands = ""
+    for hand in state.hand_data:
+        hands += "\n%s   %d" % [hand.name.replace("\n", " "), hand.played]
+    $TabContainer/Status/Label2.text = hands
 
 func _on_continue_pressed() -> void:
     self.visible = false
