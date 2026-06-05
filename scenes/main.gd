abd48d23f78e4aacc1056437a0eaa7e1456936cf
Miguel Rojas <leprosy57@gmail.com>
2026-06-05 18:01:52 -0400

    Saving data...for now, max round

--- scenes/main.gd
+++ scenes/main.gd
@@ -9,11 +9,13 @@ var scenes = {
     "credits": preload("res://scenes/screens/credits.tscn"),
     "help": preload("res://scenes/screens/help.tscn")
 }
+var user_data
 
 func _ready() -> void:
     print("Main: ready")
     $Music.play_rnd()
-    set_active_scene("main_menu")
+    self.check_saved_data()
+    self.set_active_scene("main_menu")
 
 func set_active_scene(node: String) -> void:
     var next_scene = scenes[node].instantiate() as Screen
@@ -22,3 +24,21 @@ func set_active_scene(node: String) -> void:
     if len(cur_scene.get_children()) > 0:
         cur_scene.remove_child(cur_scene.get_children()[0])
     cur_scene.add_child(next_scene)
+
+func check_saved_data() -> void:
+    if not FileAccess.file_exists("user://user_data.save"):
+        print("No saved data found, creating new savegame")
+        self.user_data = {"max_round": 0}
+        self.update_user_data()
+    else:
+        print("Saved data found, loading saved data")
+        var file = FileAccess.open("user://user_data.save", FileAccess.READ)
+        self.user_data = file.get_var()
+        file.close()
+    print(self.user_data)
+    print("Data loaded")
+
+func update_user_data() -> void:
+    var file = FileAccess.open("user://user_data.save", FileAccess.WRITE)
+    file.store_var(self.user_data)
+    file.close()
--- scenes/screens/game_over.gd
+++ scenes/screens/game_over.gd
@@ -2,9 +2,20 @@ extends Screen
 
 func _ready() -> void:
     var state = $"../../State"
-    state.reset_state(false)
+    var user_data = self.Main.user_data
+    print(user_data)
     self.Main.get_node("Music").stop()
+
+    if state.round > user_data.max_round:
+        $Control/Panel/RecordLabel.visible = true
+        user_data.max_round = state.round
+        self.Main.update_user_data()
+    else:
+        $Control/Panel/RecordLabel.visible = false
+
+    $Control/Panel/Label.text = "You reached round %s" % state.round
     $AudioStreamPlayer.play()
+    state.reset_state(false)
 
 func _on_button_pressed() -> void:
     self.Main.get_node("Music").play_rnd()
